cdfeff3c33365f2bd8d14102446c2332a5364026
Konteh <kontehebrima993@gmail.com>
2021-03-29 13:22:32 -0700

    Add the form query to search for city

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const { response } = require("express");
 const express = require("express");
-const app = express();
 const https = require("https");
-const { url } = require("inspector");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/" ,(req,res)=>{
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=bfdeecfeeca4d5e7813fded2fd430cc9&units=metric";
-    https.get(url, (response)=>{
-        console.log(response.statusCode);
-
-        response.on("data",function(data){
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp 
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("<p> The weather description is " + weatherDescription + "</p>")
-            res.write("<h1> The temperature in London is " + temp + " degree celcius</h1>")
-            res.write("<img src="+ imageURL + ">")
-            res.send
-        })
-    })
+    res.sendFile(__dirname + "/index.html");
   
 })
+app.post("/" , (req,res)=>{
+            const query = req.body.cityName; 
+            const apikey = "bfdeecfeeca4d5e7813fded2fd430cc9";
+            const unit = "metric";
+            const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+ apikey +"&units=" + unit;
+            https.get(url, (response)=>{
+                console.log(response.statusCode);
+
+                response.on("data",function(data){
+                    const weatherData = JSON.parse(data)
+                    const temp = weatherData.main.temp 
+                    const weatherDescription = weatherData.weather[0].description
+                    const icon = weatherData.weather[0].icon
+                    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+                    res.write("<p> The weather description is " + weatherDescription + "</p>")
+                    res.write("<h1> The temperature in "+query+" is " + temp + " degree celcius</h1>")
+                    res.write("<img src="+ imageURL + ">")
+                    res.send
+                })
+            })
+})
+
+ 
 
 
 app.listen(3000, ()=>{
